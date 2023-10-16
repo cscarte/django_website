24c9dcc0d386e7c1767c9c13deb30709f8b45e5a
ccarter1990 <ccar491@wgu.edu>
2023-10-15 21:25:34 -0500

    10/15/23

--- base/models.py
+++ base/models.py
@@ -4,7 +4,13 @@ from django.db import models
 # Create your models here.
 
 class Room(models.Model):
-    #host = models.ForeignKey(User, on_delete=models.CASCADE)
-    #topic = models.CharField(max_length=255)
+    # host = models.ForeignKey(User, on_delete=models.CASCADE)
+    # topic = models.CharField(max_length=255)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True, max_length=5000)
+    # participants = models.ManyToManyField('Participant', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
